852e4cce59046da8d3a7f15d48fe03a882115051
Yoon Jun Pil <galaxy160102@gmail.com>
2023-05-15 23:39:14 +0900

    feat : project build

---
+++ src/main/java/com/example/springbootproj01/SpringbootProj01Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootproj01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProj01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootProj01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootproj01/SpringbootProj01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootproj01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProj01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
